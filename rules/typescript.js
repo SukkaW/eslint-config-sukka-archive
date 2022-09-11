6c0696c9d689e896bd7e042729390291cfa26953
SukkaW <isukkaw@gmail.com>
2022-09-11 16:44:15 +0800

    rule: allow void return in promise callback

--- rules/typescript.js
+++ rules/typescript.js
@@ -24,7 +24,7 @@ module.exports = {
   '@typescript-eslint/require-await': 'error',
   '@typescript-eslint/await-thenable': 'error',
   '@typescript-eslint/no-for-in-array': 'error',
-  '@typescript-eslint/no-misused-promises': 'error',
+  '@typescript-eslint/no-misused-promises': ['error', { checksVoidReturn: false }],
   '@typescript-eslint/no-unnecessary-type-assertion': 'error',
   '@typescript-eslint/no-unsafe-call': 'error',
   '@typescript-eslint/no-unsafe-member-access': 'off',
